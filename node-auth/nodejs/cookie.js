dd586bdc7b67ff042aa0c56af0c00b820373310e
powerjaehoonzz <powerjaehoon@gmail.com>
2026-02-09 22:37:31 +0900

    feat: add cookie validation

--- node-auth/nodejs/cookie.js
+++ node-auth/nodejs/cookie.js
@@ -1,7 +1,18 @@
 var http = require("http");
+var cookie = require("cookie");
+
 http
   .createServer(function (req, res) {
+    console.log(req.headers.cookie);
+    if (!req.headers.cookie) {
+      res.writeHead(401);
+      res.end("No Cookie!!");
+      return;
+    }
+    var cookies = cookie.parse(req.headers.cookie);
+    console.log(cookies.yummy_cookie);
     res.writeHead(200, {"Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"]});
+
     res.end("Cookie!!");
   })
   .listen(3000);
