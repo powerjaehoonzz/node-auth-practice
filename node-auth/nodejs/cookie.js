307007bf771bb19afb60380b332997fd5f29058c
powerjaehoonzz <powerjaehoon@gmail.com>
2026-02-10 12:18:26 +0900

    feat: add persistent cookie using Max-Age

--- node-auth/nodejs/cookie.js
+++ node-auth/nodejs/cookie.js
@@ -4,15 +4,18 @@ var cookie = require("cookie");
 http
   .createServer(function (req, res) {
     console.log(req.headers.cookie);
-    if (!req.headers.cookie) {
-      res.writeHead(401);
-      res.end("No Cookie!!");
-      return;
+    var cookies = {};
+    if (req.headers.cookie !== undefined) {
+      cookies = cookie.parse(req.headers.cookie);
     }
-    var cookies = cookie.parse(req.headers.cookie);
     console.log(cookies.yummy_cookie);
-    res.writeHead(200, {"Set-Cookie": ["yummy_cookie=choco", "tasty_cookie=strawberry"]});
-
+    res.writeHead(200, {
+      "Set-Cookie": [
+        "yummy_cookie=choco",
+        "tasty_cookie=strawberry",
+        `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+      ],
+    });
     res.end("Cookie!!");
   })
   .listen(3000);
