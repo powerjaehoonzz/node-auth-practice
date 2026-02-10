455a9faaf3aa14bf19137f29b9c137015ebfd6a6
powerjaehoonzz <powerjaehoon@gmail.com>
2026-02-10 15:46:31 +0900

    feat: implement basic login flow with cookie-based authentication

--- node-auth/nodejs/cookie.js
+++ node-auth/nodejs/cookie.js
@@ -11,6 +11,9 @@ http
         "yummy_cookie=choco",
         "tasty_cookie=strawberry",
         `Permanent=cookies; Max-Age=${60 * 60 * 24 * 30}`,
+        "Secure=Secure; Secure",
+        "HttpOnly=HttpOnly; HttpOnly",
+        "Path=Path; Path=/cookie",
       ],
     });
     res.end("Cookie!!");
