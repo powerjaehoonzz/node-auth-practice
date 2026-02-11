3e729865605cc569b55c696c32406e43002d677c
powerjaehoonzz <powerjaehoon@gmail.com>
2026-02-12 01:40:27 +0900

    feat: add logout route and clear session

--- express-session/routes/auth.js
+++ express-session/routes/auth.js
@@ -43,80 +43,10 @@ router.post("/login_process", (req, res) => {
   });
 });
 
-router.get("/update/:pageId", (req, res) => {
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", function (err, description) {
-    const title = req.params.pageId;
-    const list = template.list(req.list);
-    const html = template.HTML(
-      title,
-      list,
-      `
-            <form action="/topic/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
-              <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-              <p>
-                <textarea name="description" placeholder="description">${description}</textarea>
-              </p>
-              <p>
-                <input type="submit">
-              </p>
-            </form>
-            `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`,
-    );
-    res.send(html);
-  });
-});
-
-router.post("/update_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const title = post.title;
-  const description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function (error) {
-    fs.writeFile(`data/${title}`, description, "utf8", function (err) {
-      res.redirect(`/topic/${title}`);
-    });
-  });
-});
-
-router.post("/delete_process", (req, res) => {
-  const post = req.body;
-  const id = post.id;
-  const filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function (error) {
+router.get("/logout", (req, res) => {
+  req.session.destroy((err) => {
     res.redirect("/");
   });
 });
 
-router.get("/:pageId", (req, res, next) => {
-  const filteredId = path.parse(req.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, "utf8", (error, description) => {
-    if (error) {
-      if (error.code === "ENOENT") {
-        return res.status(404).send("404 Not Found");
-      }
-      return next(error);
-    }
-    const title = req.params.pageId;
-    const sanitizedTitle = sanitizeHtml(title);
-    const sanitizedDescription = sanitizeHtml(description, {
-      allowedTags: ["h1"],
-    });
-    const list = template.list(req.list);
-    const html = template.HTML(
-      sanitizedTitle,
-      list,
-      `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-      ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
-            <form action="/topic/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`,
-    );
-    res.send(html);
-  });
-});
 module.exports = router;
